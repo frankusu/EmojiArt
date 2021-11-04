7c24cc4928da8c2e98ee92576a95c904fba304e6
Frank Su <frank.su@applydigital.co>
2021-11-04 13:30:31 -0700

    Project setup

---
+++ EmojiArt/EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Frank Su on 2021-11-04.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
